41d0d4c06c04b6e4dadc905b157894640c079e97
Nedim Kulašin <kulasinn@gmail.com>
2025-11-29 11:56:48 +0100

    commit2

--- frontend/App.js
+++ frontend/App.js
@@ -1,48 +1,15 @@
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { GluestackUIProvider } from '@gluestack-ui/themed';
 import { config } from '@gluestack-ui/config';
-import WelcomeScreen from './src/screens/WelcomeScreen';
-import LoginScreen from './src/screens/LoginScreen';
-import RegisterScreen from './src/screens/RegisterScreen';
-import HomeScreen from './src/screens/HomeScreen';
-import PollDetailScreen from './src/screens/PollDetailScreen';
-import SubscriptionScreen from './src/screens/SubscriptionScreen';
-import AnonymousInboxScreen from './src/screens/AnonymousInboxScreen';
-import CreatePollScreen from './src/screens/CreatePollScreen';
-import ProfileScreen from './src/screens/ProfileScreen';
-import SendAnonymousMessageScreen from './src/screens/SendAnonymousMessageScreen';
-import ShareLinkScreen from './src/screens/ShareLinkScreen';
 import MainTabs from './src/navigation/MainTabs';
-
-const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <GluestackUIProvider config={config}>
       <NavigationContainer>
-        <Stack.Navigator 
-          initialRouteName="Welcome"
-          screenOptions={{
-            headerShown: false,
-            animation: 'default',
-          }}
-        >
-          <Stack.Screen name="Welcome" component={WelcomeScreen} />
-          <Stack.Screen name="Login" component={LoginScreen} />
-          <Stack.Screen name="Register" component={RegisterScreen} />
-          <Stack.Screen name="MainTabs" component={MainTabs} />
-          <Stack.Screen name="Home" component={HomeScreen} />
-          <Stack.Screen name="CreatePoll" component={CreatePollScreen} />
-          <Stack.Screen name="PollDetail" component={PollDetailScreen} />
-          <Stack.Screen name="Subscription" component={SubscriptionScreen} />
-          <Stack.Screen name="Profile" component={ProfileScreen} />
-          <Stack.Screen name="AnonymousInbox" component={AnonymousInboxScreen} />
-          <Stack.Screen name="SendAnonymousMessage" component={SendAnonymousMessageScreen} />
-          <Stack.Screen name="ShareLink" component={ShareLinkScreen} />
-        </Stack.Navigator>
+        <MainTabs />
         <StatusBar style="auto" />
       </NavigationContainer>
     </GluestackUIProvider>
