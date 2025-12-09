24af345afe922263a8da8743c665bd825480c9d8
Nedim Kulašin <kulasinn@gmail.com>
2025-12-09 01:06:29 +0100

    htaccess

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const baseURL = process.env.REACT_NATIVE_API_URL || 'http://hajp.app/backend/public';
+export const baseURL = process.env.REACT_NATIVE_API_URL || 'http://hajp.app';
 // export const baseURL = process.env.REACT_NATIVE_API_URL || 'http://192.168.0.26:8000';
 
 const api = axios.create({ baseURL });
